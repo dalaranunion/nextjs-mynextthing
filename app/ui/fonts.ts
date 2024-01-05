cd04169959a817f103491070f8ef590bb56cf8cd
Angelos <panatha_1989@hotmail.com>
2024-01-05 14:51:57 +0000

    Chapter

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  weight: ['300', '400', '600'],
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400'],
+  subsets: ['latin'],
+});
